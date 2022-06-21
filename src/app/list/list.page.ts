8580c00943559991ef1ac7eae76d7d433ee85c10
Matthieu Mota <matthieumota@gmail.com>
2022-06-21 14:37:32 +0200

    Infinite scroll

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -12,10 +12,29 @@ export class ListPage {
   constructor(private http: HttpClient) {}
 
   ionViewWillEnter() {
-    this.http.get('https://randomuser.me/api/?results=100').subscribe(response => {
+    this.http.get('https://randomuser.me/api/?results=10').subscribe(response => {
       console.log(response);
       this.items = response['results'];
     });
   }
 
+  loadData(event) {
+    // On fait une requête AJAX et on arrête le loader
+    console.log(event);
+    // Le code à faire... AJAX + Ajout dans le tableau
+    this.http.get('https://randomuser.me/api/?results=10').subscribe(response => {
+      this.items = this.items.concat(response['results']);
+      event.target.complete();
+
+      if (this.items.length >= 200) {
+        event.target.disabled = true; // Je désactive l'infinite scroll
+      }
+    });
+
+    setTimeout(() => {
+      // this.items.push(this.items[0]);
+      // var n = [1, 2, 3].concat([4, 5, 6]);
+      // event.target.complete();
+    }, 500);
+  }
 }
